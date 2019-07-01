47ca7ec5c3c76265b0beb59797e2af9b396c8dc3
Ethan Soergel <esoergel@gmail.com>
2019-07-01 17:31:39 -0400

    Add migration to populate new fields

---
+++ corehq/apps/app_manager/migrations/0004_multi_master_linked_apps.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.db import migrations
+
+from corehq.apps.app_manager.models import LinkedApplication
+from corehq.dbaccessors.couchapps.all_docs import get_doc_ids_by_class
+from corehq.util.couch import DocUpdate, iter_update
+from corehq.util.log import with_progress_bar
+
+
+def _populate_linked_app_fields(apps, schema_editor):
+    app_ids = get_doc_ids_by_class(LinkedApplication)
+    iter_update(LinkedApplication.get_db(), _add_fields, with_progress_bar(app_ids))
+
+
+def _add_fields(app_doc):
+    app_doc['progenitor_id'] = app_doc['master']
+    app_doc['pulled_from_master_app_id'] = app_doc['master']
+    app_doc['pulled_from_master_version'] = app_doc['version']
+    return DocUpdate(app_doc)
+
+
+def _reverse_noop(app, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app_manager', '0003_auto_20190326_0853'),
+    ]
+
+    operations = [
+        migrations.RunPython(_populate_linked_app_fields,
+                             reverse_code=migrations.RunPython.noop),
+    ]
