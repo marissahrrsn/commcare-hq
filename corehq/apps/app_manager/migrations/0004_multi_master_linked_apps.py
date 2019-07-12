e5d8ff355ea066fc197835a286aeaa972e84c263
Ethan Soergel <esoergel@gmail.com>
2019-07-12 14:49:31 -0400

    Migrate also deleted linked apps

--- corehq/apps/app_manager/migrations/0004_multi_master_linked_apps.py
+++ corehq/apps/app_manager/migrations/0004_multi_master_linked_apps.py
@@ -3,15 +3,19 @@ from __future__ import absolute_import, unicode_literals
 from django.db import migrations
 
 from corehq.apps.app_manager.models import LinkedApplication
-from corehq.dbaccessors.couchapps.all_docs import get_doc_ids_by_class
-from corehq.util.django_migrations import skip_on_fresh_install
+from corehq.dbaccessors.couchapps.all_docs import (
+    get_deleted_doc_ids_by_class,
+    get_doc_ids_by_class,
+)
 from corehq.util.couch import DocUpdate, iter_update
+from corehq.util.django_migrations import skip_on_fresh_install
 from corehq.util.log import with_progress_bar
 
 
 @skip_on_fresh_install
 def _populate_linked_app_fields(apps, schema_editor):
-    app_ids = get_doc_ids_by_class(LinkedApplication)
+    app_ids = (get_doc_ids_by_class(LinkedApplication)
+               + get_deleted_doc_ids_by_class(LinkedApplication))
     iter_update(LinkedApplication.get_db(), _add_fields, with_progress_bar(app_ids))
 
 
